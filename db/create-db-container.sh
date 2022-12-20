5dcb4431a6508da39868bf8d83bd28a759d43cfb
Ivan Zykov <ivan.v.zykov@gmail.com>
2022-12-20 12:22:24 +0100

    Adds script to create DB container

---
+++ db/create-db-container.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+docker container create \
+  --name craftworks-db \
+  -e POSTGRES_PASSWORD=sample \
+  -e POSTGRES_USER=sample \
+  -e POSTGRES_DB=sample \
+  -e PGDATA=/var/lib/postgresql/data/pgdata \
+  -p 127.0.0.1:5432:5432 \
+  postgres:latest
