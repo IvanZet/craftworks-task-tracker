dadd9cd004d430957450c9013a712634e11f2b0e
Ivan Zykov <ivan.v.zykov@gmail.com>
2022-12-21 12:35:36 +0100

    Adds initial Spring Boot files

---
+++ src/test/java/net/ivanzykov/craftworkstasktracker/CraftworksTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ivanzykov.craftworkstasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftworksTaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
