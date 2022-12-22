6040cbd4b0090c9eac001b549d1aec3909ca9df9
Ivan Zykov <ivan.v.zykov@gmail.com>
2022-12-22 17:37:12 +0100

    Adds JPA entity for tasks table

---
+++ src/main/java/net/ivanzykov/craftworkstasktracker/Task.java
@@ -0,0 +1,122 @@
+package net.ivanzykov.craftworkstasktracker;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "tasks")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    @Column(name = "due_date")
+    @Temporal(TemporalType.DATE)
+    private Date dueDate;
+
+    @Column(name = "resolved_at")
+    @Temporal(TemporalType.DATE)
+    private Date resolvedAt;
+
+    private String title;
+    private String description;
+    private String priority;
+    private String status;
+
+    protected Task() {}
+
+    public Task(Date createdAt, Date updatedAt, Date dueDate, Date resolvedAt, String title, String description, String priority, String status) {
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.dueDate = dueDate;
+        this.resolvedAt = resolvedAt;
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public java.util.Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(java.util.Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public java.util.Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(java.util.Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getResolvedAt() {
+        return resolvedAt;
+    }
+
+    public void setResolvedAt(Date resolvedAt) {
+        this.resolvedAt = resolvedAt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
