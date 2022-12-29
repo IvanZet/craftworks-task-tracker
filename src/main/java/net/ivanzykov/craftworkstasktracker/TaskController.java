a04587569a73f2c9400e0dab0e1596dad958fcf2
Ivan Zykov <ivan.v.zykov@gmail.com>
2022-12-29 17:56:09 +0100

    Adds controller for tasks

---
+++ src/main/java/net/ivanzykov/craftworkstasktracker/TaskController.java
@@ -0,0 +1,47 @@
+package net.ivanzykov.craftworkstasktracker;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    List<Task> fetchAll() {
+        return taskRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    Task fetchSingle (@PathVariable Long id) {
+        // TODO: if not found, throw EmployeeNotFoundException like described in
+        // https://spring.io/guides/tutorials/rest/
+        return taskRepository.getReferenceById(id);
+    }
+
+    // FIXME: use DTO for task arg
+    @PostMapping
+    Task createSingle(@RequestBody final Task task) {
+        return taskRepository.saveAndFlush(task);
+    }
+
+    // FIXME: use DTO for task arg
+    @PutMapping("{id}")
+    Task updateSingle(@PathVariable Long id, @RequestBody Task updatedTask) {
+        // TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
+        Task oldTask = taskRepository.getReferenceById(id);
+        BeanUtils.copyProperties(updatedTask, oldTask, "id");
+        return taskRepository.saveAndFlush(oldTask);
+    }
+
+    @DeleteMapping("{id}")
+    void deleteSingle(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+}
