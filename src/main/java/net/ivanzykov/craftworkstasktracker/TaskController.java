f24551a112ff82c1bd6cd474b0560826b62d417c
Ivan Zykov <ivan.v.zykov@gmail.com>
2023-01-02 17:05:07 +0100

    Move business logic from controller to service

--- src/main/java/net/ivanzykov/craftworkstasktracker/TaskController.java
+++ src/main/java/net/ivanzykov/craftworkstasktracker/TaskController.java
@@ -1,6 +1,5 @@
 package net.ivanzykov.craftworkstasktracker;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,36 +10,36 @@ import java.util.List;
 public class TaskController {
 
     @Autowired
-    private TaskRepository taskRepository;
+    private TaskService taskService;
 
     @GetMapping
+    // FIXME: use DTO for task arg
     List<Task> fetchAll() {
-        return taskRepository.findAll();
+        return taskService.fetchAll();
     }
 
     @GetMapping("{id}")
     Task fetchSingle (@PathVariable Long id) {
-        return taskRepository.findById(id)
+        return taskService.fetchSingle(id)
                 .orElseThrow(() -> new TaskNotFoundException(id));
     }
 
     // FIXME: use DTO for task arg
     @PostMapping
     Task createSingle(@RequestBody final Task task) {
-        return taskRepository.saveAndFlush(task);
+        // TODO: check that required fields passed, otherwise return a 400 bad payload
+        return taskService.createSingle(task);
     }
 
     // FIXME: use DTO for task arg
     @PutMapping("{id}")
     Task updateSingle(@PathVariable Long id, @RequestBody Task updatedTask) {
-        // TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
-        Task oldTask = taskRepository.getReferenceById(id);
-        BeanUtils.copyProperties(updatedTask, oldTask, "id");
-        return taskRepository.saveAndFlush(oldTask);
+        return taskService.updateSingle(id, updatedTask);
     }
 
     @DeleteMapping("{id}")
     void deleteSingle(@PathVariable Long id) {
-        taskRepository.deleteById(id);
+        // TODO: handle when task not found
+        taskService.deleteSingle(id);
     }
 }
---
+++ src/main/java/net/ivanzykov/craftworkstasktracker/TaskServiceImpl.java
@@ -0,0 +1,68 @@
+package net.ivanzykov.craftworkstasktracker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    @Autowired
+    private final TaskRepository taskRepository;
+
+    TaskServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public List<Task> fetchAll() {
+        return taskRepository.findAll();
+    }
+
+    @Override
+    public Optional<Task> fetchSingle(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    @Override
+    public Task createSingle(Task task) {
+        return taskRepository.saveAndFlush(task);
+    }
+
+    @Override
+    public Task updateSingle(Long id, Task updatedTask) {
+        // TODO: rework updating fields via DTO
+        // TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
+        Task oldTask = fetchSingle(id)
+                .orElseThrow(() -> new TaskNotFoundException(id));
+
+        oldTask.setUpdatedAt(updatedTask.getUpdatedAt());
+        if (updatedTask.getDueDate() != null) {
+            oldTask.setDueDate(updatedTask.getDueDate());
+        }
+        oldTask.setResolvedAt(updatedTask.getResolvedAt());
+        if (updatedTask.getTitle() != null) {
+            oldTask.setTitle(updatedTask.getTitle());
+        }
+        oldTask.setDescription(updatedTask.getDescription());
+        if (updatedTask.getPriority() != null) {
+            oldTask.setPriority(updatedTask.getPriority());
+        }
+        if (updatedTask.getStatus() != null) {
+            oldTask.setPriority(updatedTask.getPriority());
+        }
+
+        return saveAndFlush(oldTask);
+    }
+
+    @Override
+    public void deleteSingle(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    private Task saveAndFlush(Task task) {
+        return taskRepository.saveAndFlush(task);
+    }
+}
