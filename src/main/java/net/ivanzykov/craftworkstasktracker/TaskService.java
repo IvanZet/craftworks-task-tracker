a0bc4042df32a8a7f56efc43ec60b2166b92a2d7
Ivan Zykov <ivan.v.zykov@gmail.com>
2023-01-04 16:21:15 +0100

    Removes unused dependencies in TaskService interface

--- src/main/java/net/ivanzykov/craftworkstasktracker/TaskService.java
+++ src/main/java/net/ivanzykov/craftworkstasktracker/TaskService.java
@@ -1,8 +1,5 @@
 package net.ivanzykov.craftworkstasktracker;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import java.util.List;
 import java.util.Optional;
 
