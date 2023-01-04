19d12540904f3d9a5e0b66b013bf62c6a1086495
Ivan Zykov <ivan.v.zykov@gmail.com>
2023-01-04 12:27:45 +0100

    Applies DTO of task for fetching all tasks

--- src/main/java/net/ivanzykov/craftworkstasktracker/TaskController.java
+++ src/main/java/net/ivanzykov/craftworkstasktracker/TaskController.java
@@ -1,8 +1,10 @@
 package net.ivanzykov.craftworkstasktracker;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.ZoneId;
 import java.util.List;
 
 @RestController
@@ -10,15 +12,21 @@ import java.util.List;
 public class TaskController {
 
     @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
     private TaskService taskService;
 
     @GetMapping
-    // FIXME: use DTO for task arg
-    List<Task> fetchAll() {
-        return taskService.fetchAll();
+    List<TaskDto> fetchAll() {
+        List<Task> tasks = taskService.fetchAll();
+        return tasks.stream()
+                .map(this::mapToDto)
+                .toList();
     }
 
     @GetMapping("{id}")
+    // FIXME: use DTO
     Task fetchSingle (@PathVariable Long id) {
         return taskService.fetchSingle(id)
                 .orElseThrow(() -> new TaskNotFoundException(id));
@@ -41,5 +49,17 @@ public class TaskController {
     void deleteSingle(@PathVariable Long id) {
         // TODO: handle when task not found
         taskService.deleteSingle(id);
+    }
+
+    private TaskDto mapToDto(Task task) {
+        TaskDto taskDto = modelMapper.map(task, TaskDto.class);
+        taskDto.setAllDatesToClientsTimeZone(
+                task.getCreatedAt(),
+                task.getUpdatedAt(),
+                task.getDueDate(),
+                task.getResolvedAt(),
+                // TODO: try getting time zone from the request
+                ZoneId.of("CET"));
+        return taskDto;
     }
 }
