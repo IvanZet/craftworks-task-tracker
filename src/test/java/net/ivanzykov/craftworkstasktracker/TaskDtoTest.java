d51f093db79cb70b3757bc9c4b8c18ef9247f830
Ivan Zykov <ivan.v.zykov@gmail.com>
2023-01-05 17:59:45 +0100

    Finishes testing parseAndConvertDate() in TaskDto

--- src/test/java/net/ivanzykov/craftworkstasktracker/TaskDtoTest.java
+++ src/test/java/net/ivanzykov/craftworkstasktracker/TaskDtoTest.java
@@ -2,9 +2,13 @@ package net.ivanzykov.craftworkstasktracker;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,13 +16,13 @@ class TaskDtoTest {
 
     private TaskDto taskDto;
 
-    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm zz");
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm zz");
 
-    private final String dateTimeOfClientString = "2023-01-04 16:00 CET";
-    private final Instant dateTimeOfClientInstant = ZonedDateTime.parse(dateTimeOfClientString, dateTimeFormatter)
+    private static final String dateTimeOfClientString = "2023-01-04 16:00 CET";
+    private static final Instant dateTimeOfClientInstant = ZonedDateTime.parse(dateTimeOfClientString, dateTimeFormatter)
             .toInstant();
-    private final ZoneId timezoneOfDb = ZoneId.of("UTC");
-    private final ZoneOffset offsetInDbTimezone = timezoneOfDb.getRules().getOffset(dateTimeOfClientInstant);
+    private static final ZoneId timezoneOfDb = ZoneId.of("UTC");
+    private static final ZoneOffset offsetInDbTimezone = timezoneOfDb.getRules().getOffset(dateTimeOfClientInstant);
 
     @BeforeEach
     void setUp() {
@@ -85,15 +89,20 @@ class TaskDtoTest {
         assertEquals(expected, taskDto.getDueDateConverted(timezoneOfDb));
     }
 
-    @Test
-    void getResolvedAtConverted() {
+    @ParameterizedTest
+    @MethodSource("provideGetResolvedAtConverted")
+    void getResolvedAtConverted(String dateTime, OffsetDateTime expected) {
         // Prepare SUT
-        taskDto.setResolvedAt(dateTimeOfClientString);
-
-        // Run this test
-        OffsetDateTime expected = OffsetDateTime.of(2023, 1, 4, 15, 0, 0,
-                0, offsetInDbTimezone);
+        taskDto.setResolvedAt(dateTime);
 
         assertEquals(expected, taskDto.getResolvedAtConverted(timezoneOfDb));
+    }
+
+    public static Stream<Arguments> provideGetResolvedAtConverted() {
+        return Stream.of(
+                Arguments.of(dateTimeOfClientString, OffsetDateTime.of(2023, 1, 4, 15, 0, 0,
+                        0, offsetInDbTimezone)),
+                Arguments.of(null, null)
+        );
     }
 }
