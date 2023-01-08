50adb9a3c174ed991f36701e7d88409648b2de42
Ivan Zykov <ivan.v.zykov@gmail.com>
2023-01-08 12:55:38 +0100

    Adds scheduler generatin same tasks

--- src/main/java/net/ivanzykov/craftworkstasktracker/CraftworksTaskTrackerApplication.java
+++ src/main/java/net/ivanzykov/craftworkstasktracker/CraftworksTaskTrackerApplication.java
@@ -2,8 +2,11 @@ package net.ivanzykov.craftworkstasktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {TaskSchedulingAutoConfiguration.class})
+@EnableScheduling
 public class CraftworksTaskTrackerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/net/ivanzykov/craftworkstasktracker/SchedulerOfTasks.java
@@ -0,0 +1,44 @@
+package net.ivanzykov.craftworkstasktracker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Component
+public class SchedulerOfTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(SchedulerOfTasks.class);
+
+    @Autowired
+    TaskService taskService;
+
+    @Value("${spring.jpa.properties.hibernate.jdbc.time_zone}")
+    private String timezoneOfDb;
+
+    @Scheduled(initialDelay = 1000, fixedRate = 10000)
+    @Transactional
+    public void scheduleCreatingTask() {
+        Task task = new Task();
+
+        ZonedDateTime nowInDbTimeZone = ZonedDateTime.now()
+                .withZoneSameInstant(ZoneId.of(timezoneOfDb));
+        OffsetDateTime dueDate = nowInDbTimeZone.plusMonths(1L).toOffsetDateTime();
+        task.setDueDate(dueDate);
+
+        task.setTitle("Another auto-generated task");
+        task.setPriority("md");
+        task.setStatus("wait");
+
+        Task taskCreated = taskService.createSingle(task);
+
+        log.info("Task auto-generated with ID: {}", taskCreated.getId());
+    }
+}
