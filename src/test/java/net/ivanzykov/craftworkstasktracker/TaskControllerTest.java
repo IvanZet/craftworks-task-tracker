1dbc1f38685801383cb2ba468694cf6970cb2e2d
Ivan Zykov <ivan.v.zykov@gmail.com>
2023-01-23 15:45:20 +0100

    Adds test for createSingle() action in controller

--- src/test/java/net/ivanzykov/craftworkstasktracker/TaskControllerTest.java
+++ src/test/java/net/ivanzykov/craftworkstasktracker/TaskControllerTest.java
@@ -2,10 +2,13 @@ package net.ivanzykov.craftworkstasktracker;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
@@ -41,12 +45,17 @@ class TaskControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private final ZoneId timezoneOfDb = ZoneId.of("UTC");
+    private final String createdAtCet = "2023-01-04 16:00 CET";
+    private final String updatedAtCet = "2023-01-04 21:00 CET";
+    private final String dueDateCet = "2023-02-01 00:00 CET";
+    private final String resolvedAtCet = "2023-01-08 18:00 CET";
+    private final String subPath = "/api/v1/tasks";
+
     @Test
-    void fetchAll_returnsOneTaskWithCorrectFields() throws Exception{
+    void fetchAll_returnsOneTaskWithCorrectFields() throws Exception {
         // Prepare args
         var task = new Task();
-
-        ZoneId timezoneOfDb = ZoneId.of("UTC");
 
         OffsetDateTime createdAt = ZonedDateTime.of(2023, 1, 4, 15, 0, 0,
                 0, timezoneOfDb).toOffsetDateTime();
@@ -74,19 +83,71 @@ class TaskControllerTest {
                 .thenReturn(List.of(task));
 
         // Run this test
-        final String subPath = "/api/v1/tasks";
-        mockMvc.perform(get(subPath))
+        mockMvc.perform(MockMvcRequestBuilders.get(subPath))
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 // Expect time in client's CET timezone
-                .andExpect(jsonPath("$[0].createdAt", is("2023-01-04 16:00 CET")))
-                .andExpect(jsonPath("$[0].updatedAt", is("2023-01-04 21:00 CET")))
-                .andExpect(jsonPath("$[0].dueDate", is("2023-02-01 00:00 CET")))
-                .andExpect(jsonPath("$[0].resolvedAt", is("2023-01-08 18:00 CET")))
+                .andExpect(jsonPath("$[0].createdAt", is(createdAtCet)))
+                .andExpect(jsonPath("$[0].updatedAt", is(updatedAtCet)))
+                .andExpect(jsonPath("$[0].dueDate", is(dueDateCet)))
+                .andExpect(jsonPath("$[0].resolvedAt", is(resolvedAtCet)))
                 .andExpect(jsonPath("$[0].title", is(task.getTitle())))
                 .andExpect(jsonPath("$[0].description", is(task.getDescription())))
                 .andExpect(jsonPath("$[0].priority", is(task.getPriority())))
                 .andExpect(jsonPath("$[0].status", is(task.getStatus())));
+    }
+
+    @Test
+    void createSingle_returnCreatedTasksWithCorrectFields() throws Exception {
+        // Prepare args
+        var taskDto = new TaskDto();
+        taskDto.setCreatedAt(createdAtCet);
+        taskDto.setUpdatedAt(updatedAtCet);
+        taskDto.setDueDate(dueDateCet);
+        taskDto.setResolvedAt(resolvedAtCet);
+        taskDto.setTitle("t");
+        taskDto.setDescription("d");
+        taskDto.setPriority("md");
+        taskDto.setStatus("work");
+
+        var task = new Task();
+        task.setCreatedAt(taskDto.getCreatedAtConverted(timezoneOfDb));
+        task.setUpdatedAt(taskDto.getUpdatedAtConverted(timezoneOfDb));
+        task.setDueDate(taskDto.getDueDateConverted(timezoneOfDb));
+        task.setResolvedAt(taskDto.getResolvedAtConverted(timezoneOfDb));
+        task.setTitle(taskDto.getTitle());
+        task.setDescription(taskDto.getDescription());
+        task.setPriority(taskDto.getPriority());
+        task.setStatus(taskDto.getStatus());
+
+        // Stub task service
+        when(taskService.createSingle(any())).thenReturn(task);
+
+        // Run this test
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post(subPath)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(taskDto)))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.createdAt", is(taskDto.getCreatedAt())))
+                .andExpect(jsonPath("$.updatedAt", is(taskDto.getUpdatedAt())))
+                .andExpect(jsonPath("$.dueDate", is(taskDto.getDueDate())))
+                .andExpect(jsonPath("$.resolvedAt", is(taskDto.getResolvedAt())))
+                .andExpect(jsonPath("$.title", is(taskDto.getTitle())))
+                .andExpect(jsonPath("$.description", is(taskDto.getDescription())))
+                .andExpect(jsonPath("$.priority", is(taskDto.getPriority())))
+                .andExpect(jsonPath("$.status", is(taskDto.getStatus())));
+    }
+
+    // Marshal request body
+    public static String asJsonString(Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            fail();
+            throw new RuntimeException(e.getLocalizedMessage());
+        }
     }
 }
